fc81391026c0548cb437fc8278ec8da2e4d70266
yourfriendlyneighbor <carlperelman1960@gmail.com>
2017-02-22 20:44:03 -0500

    made changes

--- server.js
+++ server.js
@@ -23,7 +23,7 @@
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended : true }));
   /* GZIP Compression */
-    app.use(compress({ threshold: 0 }));
+    app.use(compress());
   /* Routes init */
     app.use('/', routes)
     app.use('/getVideos', routes);
